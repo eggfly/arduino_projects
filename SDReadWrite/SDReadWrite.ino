9a519a9c26d17f10a1fb4e9dcfc6277819b13f11
eggfly <lihaohua90@gmail.com>
2014-07-20 15:09:08 +0800

    dir tree
    Signed-off-by: eggfly <lihaohua90@gmail.com>

--- SDReadWrite/SDReadWrite.ino
+++ SDReadWrite/SDReadWrite.ino
@@ -17,55 +17,55 @@
  This example code is in the public domain.
  	 
  */
-
+ 
 #include <SD.h>
 
 File myFile;
-char bytes[1024];
-int offset = 0;
 
 void setup()
 {
-  // Open serial communications and wait for port to open:
+ // Open serial communications and wait for port to open:
   Serial.begin(9600);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
 
 
-  Serial.println("Initializing SD card...");
+  Serial.print("Initializing SD card...");
   // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
   // Note that even if it's not used as the CS pin, the hardware SS pin 
   // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
   // or the SD library functions will not work. 
-  pinMode(10, OUTPUT);
-
+   pinMode(10, OUTPUT);
+   
   if (!SD.begin(4)) {
     Serial.println("initialization failed!");
-    delay(1000);
     return;
   }
   Serial.println("initialization done.");
-
-// re-open the file for reading:
-  myFile = SD.open("HyperChem.rar");
+  
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  // re-open the file for reading:
+  myFile = SD.open("PUTTY.exe");
   if (myFile) {
-    Serial.println("HyperChem.rar:");
+    Serial.println("iToolsSetup_1.8.4.0.zip");
     // read from the file until there's nothing else in it:
     while (myFile.available()) {
-      myFile.read(bytes, 1024);
-      offset += 1024;
-      Serial.println(offset);
+        myFile.read();
+    	Serial.write("r");
     }
     // close the file:
     myFile.close();
-  } 
-  else {
-    // if the file didn't open, print an error:
+  } else {
+  	// if the file didn't open, print an error:
     Serial.println("error opening test.txt");
   }
 }
 
 void loop()
 {
-  // nothing happens after setup
+	// nothing happens after setup
 }
 
 
